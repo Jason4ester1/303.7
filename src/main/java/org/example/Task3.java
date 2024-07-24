5d62269c99d11c759719c60da42396770d0e1fec
Jason Forrester <jason4ester1@gmail.com>
2024-07-24 09:26:50 -0500

    PA 303.7.1 submission

---
+++ src/main/java/org/example/Task3.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[]args){
+        String[] colors = {"red","green","blue","yellow"};
+        String[] clonedColers = colors.clone();
+        System.out.println("Cloned array"+ Arrays.toString(clonedColers));
+    }
+}
